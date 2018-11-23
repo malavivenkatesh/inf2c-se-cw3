84eaebe03167b1178abd36f0bef1225a503fc835
Malavika Venkatesh <s1709617@evilemo.inf.ed.ac.uk>
2018-11-23 17:17:03 +0000

    add constructors for Buyer and Seller

--- src/auctionhouse/Buyer.java
+++ src/auctionhouse/Buyer.java
@@ -1,5 +1,16 @@
 package auctionhouse;
 
 public class Buyer {
-
+    
+    private String name;
+    private String address;
+    private String bankAccount;
+    private String bankAuthCode;
+    
+    public Buyer(String name,String address, String bankAccount, String bankAuthCode) {
+        this.name = name;
+        this.address = address;
+        this.bankAccount = bankAccount;
+        this.bankAuthCode = bankAuthCode;
+    }
 }
--- src/auctionhouse/Seller.java
+++ src/auctionhouse/Seller.java
@@ -1,5 +1,13 @@
 package auctionhouse;
 
 public class Seller {
-
+    private String name;
+    private String address;
+    private String bankAccount;
+    
+    public Seller(String name,String address, String bankAccount, String bankAuthCode) {
+        this.name = name;
+        this.address = address;
+        this.bankAccount = bankAccount;
+    }
 }
